34ad63ec376feaa0336d8a6b8e229d8bf99317b8
ualvesdias <ulisses.alves@protonmail.com>
2021-07-26 20:52:43 -0300

    Update crlfbruter.py

--- crlfbruter.py
+++ crlfbruter.py
@@ -8,7 +8,7 @@ r.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 def loadList():
-    
+
     print('Building URLs to test. Please, wait...')
     wordqueue = queue.Queue()
 
@@ -45,11 +45,14 @@ def bruter(wordqueue, headername):
 
         try:
             resp = r.get(attempt, verify=False, allow_redirects=False)
-            
+
             eheader = resp.headers[headername]
             if eheader != '':
                 print(f'[+] URL {url} is vulnerable with the payload {payload}')
-                
+                if args.output:
+                    with open(args.output, 'a') as outfile:
+                        outfile.write(attempt+'\n')
+
         except KeyError:
             pass
         except KeyboardInterrupt:
@@ -64,28 +67,30 @@ def bruter(wordqueue, headername):
 if __name__ == '__main__':
     parser = ap.ArgumentParser(description='A tool to test for CRLF injection')
     urlgroup = parser.add_mutually_exclusive_group(required=True)
-    urlgroup.add_argument('-u', '--url', 
+    urlgroup.add_argument('-u', '--url',
         help='One URL to be tested.')
-    urlgroup.add_argument('-ul', '--urllist', 
+    urlgroup.add_argument('-ul', '--urllist',
         help='A file containing a list of URLs to be tested.', type=ap.FileType(mode='r', encoding='latin'))
     payloadgroup = parser.add_mutually_exclusive_group(required=True)
-    payloadgroup.add_argument('-pl', '--payloadlist', type=ap.FileType(mode='r', encoding='latin'),
-        help='A file containing a list of payloads to be tested.')
     payloadgroup.add_argument('-p', '--payload', type=ap.FileType(mode='r', encoding='latin'),
         help='One payload to be used.')
-    parser.add_argument('-t', '--threads', default=1, type=int, 
+    payloadgroup.add_argument('-pl', '--payloadlist', type=ap.FileType(mode='r', encoding='latin'),
+            help='A file containing a list of payloads to be tested.')
+    parser.add_argument('-t', '--threads', default=1, type=int,
         help='The number of threads to be used. Default is 1.')
-    parser.add_argument('-hn', '--headername', type=str, 
+    parser.add_argument('-hn', '--headername', type=str,
         help='The name of the fake header.', required=True)
+    parser.add_argument('-o', '--output', type=str,
+        help='A file to save the results for the vulnerable targets.')
     args = parser.parse_args()
 
     if args.url and not args.url.startswith('http'):
         args.url = 'http://'+args.url
-    
+
     wordqueue = loadList()
 
     for _ in range(args.threads):
-        th.Thread(target=bruter, 
+        th.Thread(target=bruter,
             args=(wordqueue, args.headername)).start()
 
     wordqueue.join()
